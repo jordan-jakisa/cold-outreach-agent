2eb8683a5c5fc357059e573376039bdf4a4b4472
Jordan Mungujakisa <72340216+jordan-jakisa@users.noreply.github.com>
2024-06-30 19:08:36 +0300

    Added ability to add own subject line for the email

--- src/main.py
+++ src/main.py
@@ -1,46 +1,54 @@
-import smtplib 
 import os
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
+import smtplib
+from email.message import EmailMessage
+
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
-session = None
 
-def send_email(receipient, body):
-    with smtplib.SMTP(host='smtp.gmail.com', port=587) as session:  
+def send_email(recipient, subject, body):
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as session:
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg['Subject'] = subject
+        msg['From'] = 'jordan.jakisa@gmail.com'
+        msg['To'] = recipient
         session.starttls()
         session.login(user="jordan.jakisa@gmail.com", password=os.getenv("EMAIL_PASSWORD"))
-        session.sendmail(from_addr="jordan.jakisa@gmail.com", to_addrs=receipient, msg=body)
+        session.sendmail(from_addr="jordan.jakisa@gmail.com", to_addrs=recipient, msg=body)
+        session.send_message(msg=msg)
+
 
 def generate_email_body(name, description, sender_information, product_description):
     prompt_template = """
-    
-    You are a sales cold email generator that writes personalized emails to potential clients.
-    
-    The client we are writing to is called {name} and this is a bried note about them: {description}
-    
-    Write a cold outreach email about the following product : {product_description}
-    
-    This is information of the addresse who is sending the emails {sender_information}
+        Create a concise and professional sales cold email targeting potential leads and clients. 
+        Details to Include: 
+        Recipient's Name: {name}
+        Pain Points and Brief Note: {description} 
+        Product Pitch: Provide a concise pitch for the product described in {product_description}. 
         
-    Instructions:
-    Only write the body of the email, do not include the subject line.
-    Do not include anything else in the output besides the email body.
-    Format the email clearly and make it look professional.
-    Make the email sound like it is coming from a real person.
-    Make the email engaging logical, and persuasive.
-    Make the email professional and respectful.
+        Sender's Information: Use the details provided in {sender_information} to personalize the sender's signature 
+        or closing remark. Present them in a professional manner.
         
+          
+        Instructions: 
+        Focus solely on composing the email body; omit the subject line. 
+        Ensure the email is clear, professional, and formatted for easy reading. 
+        Limit the email to 100 words to maintain brevity and directness.
+        
+        Objective:
+        Craft a compelling email that addresses the recipient's needs, introduces the product effectively, and 
+        encourages a response or further engagement. 
+        Remember, the goal is to generate interest in the product while keeping the message succinct and targeted.
+                
     """
-    
+
     prompt = PromptTemplate.from_template(template=prompt_template)
-    
+
     llm = ChatOpenAI()
-    
+
     chain = prompt | llm | StrOutputParser()
-    response = chain.invoke(input={"product_description": product_description, "name": name, "description": description, "sender_information": sender_information})
+    response = chain.invoke(input={"product_description": product_description, "name": name, "description": description,
+                                   "sender_information": sender_information})
     return response
-    
-    
-    
-    
--- src/script.py
+++ src/script.py
@@ -1,7 +1,7 @@
-
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from dotenv import load_dotenv
+
 from main import send_email, generate_email_body
 from sidebar import sidebar
 
@@ -9,32 +9,63 @@ load_dotenv()
 
 sidebar()
 
-st.title("📨 Cold email outreach generator")
+st.title("📨 Cold email outreach AI agent")
 
-uploaded_file = st.file_uploader(label="Choose your csv file", type=['csv'],help="Please upload a csv file with the following columns: name, email, person_descritption")
+uploaded_file = st.file_uploader(
+    label="Choose your csv file",
+    type=['csv'],
+    help="Please upload a csv file with the following columns: name, email, description")
 
 if not uploaded_file:
-    st.info("Please upload a csv file with the following columns: name, email, person_descritption. You can download a template from below!")
+    st.info(
+        """
+        Please upload a csv file with the following columns: name, email, description. You can download a template
+        from [here](templates/mailing_list_template.csv)!
+        """
+    )
 else:
     df = pd.read_csv(uploaded_file)
-    
+
     form = st.form(key='input fields')
-    product_description = form.text_area("What is the product description?", placeholder="Example Wireless Earbuds 2.0, Premium sound in a wire-free design. Rich audio, 8-hour battery, touch controls, water-resistant. Charging case adds 24 hours. Perfect for workouts or daily use. Available in black, white, navy.")
-    personal_information = form.text_input(label="Please provide a name, position and name of company to be used as the adressee?", placeholder="John Wokorach, Job Position, Company", help="Separate the information using commas e.g Jordan, Software Developer, Remote Squad")
-            
+    product_description = form.text_area(label="What is the product description?",
+                                         placeholder="Example Wireless Earbuds 2.0, Premium sound in a wire-free "
+                                                     "design. Rich audio, 8-hour battery, touch controls, "
+                                                     "water-resistant. Charging case adds 24 hours. Perfect for "
+                                                     "workouts or daily use. Available in black, white, navy.")
+    personal_information = form.text_input(
+        label="Please provide a name, position and name of company to be used as the addressed?",
+        placeholder="John Wokorach, Job Position, Company",
+        help="Separate the information using commas e.g Jordan, Software Developer, Remote Squad")
+
+    subject_line = form.text_input(label="Enter a subject line for the email", )
+
     send_generate_emails = form.form_submit_button(label="Generate and send emails", use_container_width=True)
-    
+
     if send_generate_emails:
         if not product_description:
             form.warning("Please provide a product description")
         elif not personal_information:
-            form.warning("Please provide a name, position and name of company to be used as the adressee")
-        else:    
+            form.warning("Please provide a name, position and name of company to be used as the addressed")
+        elif not subject_line:
+            form.warning("Please provide a subject line for the email")
+        else:
             for index, row in df.iterrows():
                 email = row['email']
                 name = row['name']
                 description = row['description']
-                
-                email_body = generate_email_body(name=name, description=description, sender_information=personal_information, product_description=product_description)
-                send_email(email, email_body)
-                st.write(f"Sent email to {name} with email at {email}")
+                pain_points = row['pain_points']
+
+                email_body = generate_email_body(name=name, description=description.join(pain_points),
+                                                 sender_information=personal_information,
+                                                 product_description=product_description)
+
+                send_email(email, subject_line, email_body)
+
+                st.write(f"""
+                    Email sent to {name} with email at {email}
+                    
+                    ---
+                    
+                    Email body:
+                    {email_body}
+                """)
